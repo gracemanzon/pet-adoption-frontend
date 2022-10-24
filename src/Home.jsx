d767d3eabed46422b25d7862e1655200ddd790a6
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-10-24 15:24:07 -0400

    add pets index to home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,19 @@
 import { PetsIndex } from "./PetsIndex";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export function Home() {
-  const pets = "this is the pets";
+  const [pets, setPets] = useState([]);
+
+  const handlePetsIndex = () => {
+    axios.get("http://localhost:3000/pets.json").then((response) => {
+      console.log(response.data);
+      setPets(response.data);
+    });
+  };
+
+  useEffect(handlePetsIndex, []);
+
   return (
     <div>
       <h2>Home</h2>
--- src/PetsIndex.jsx
+++ src/PetsIndex.jsx
@@ -2,7 +2,15 @@ export function PetsIndex(props) {
   return (
     <div>
       <h2>All the Pets</h2>
-      {props.pets}
+      {props.pets.map((pet) => (
+        <div key={pet.id} id="pets-index" className="pets-index">
+          <img src={pet.image} alt={pet.name} />
+          <h3>{pet.name}</h3>
+          <p>Age: {pet.age} years</p>
+          <p> Breed: {pet.breed}</p>
+          <p>About: {pet.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
